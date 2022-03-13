5539714a1719d4838cca81306ae6dca69a8bc252
midibyte <midibyte_gitlab@dynamoplum.com>
2022-03-13 03:18:05 -0400

    in progress

--- alice.cpp
+++ alice.cpp
@@ -7,101 +7,278 @@
 #include <stdlib.h>
 #include <tomcrypt.h>
 #include <fstream>
+#include <stdio.h>
+#include <iterator>
+
+using namespace std;
+
+//===================SHA256 Function=========================================
+unsigned char* hashSHA2(const string& input)
+{
+	unsigned char* hash_res = new unsigned char[sha256_desc.hashsize];
+    hash_state md;
+    sha256_init(&md);
+    sha256_process(&md, (const unsigned char*) input.c_str(), input.size());
+    sha256_done(&md, hash_res);
+    return hash_res;
+}
+//----------------------------------------------------------------------------------
+//===================HMAC Function based on SHA256=========================================
+// void HMAC_Computation(char *message, unsigned char *mac, unsigned char *key)
+void HMAC_Computation(unsigned char *message, unsigned char *mac, unsigned char *key)
+{
+    int idx;
+    hmac_state hmac;
+    unsigned char dst[MAXBLOCKSIZE];
+    unsigned long dstlen;
+    register_hash(&sha256_desc);
+    idx = find_hash("sha256");
+    hmac_init(&hmac, idx, key, 32);
+    hmac_process(&hmac, (const unsigned char*) message, sizeof(message));
+    dstlen = sizeof(dst);
+    hmac_done(&hmac, dst, &dstlen);
+    memcpy(mac, dst, dstlen);
+}
+//---------------------------------------------------------------------------------
+//===================AES Encryption in CTR mode=========================================
+unsigned char* AESctr_encrypt(unsigned char *buffer, unsigned char *key)
+{
+    // hardcoded IV
+    unsigned char IV[32] = "abcdefghijklmnopqrstuvwxyzabcde";
+		symmetric_CTR ctr;
+    int x;
+    /* register AES first */
+   register_cipher(&aes_desc);
+   ctr_start(
+             find_cipher("aes"),    /* index of desired cipher */
+             IV,                        /* the initial vector */
+             key,                       /* the secret key */
+             32,                        /* length of secret key (16 bytes) */
+             0,                         /* 0 == default # of rounds */
+             CTR_COUNTER_LITTLE_ENDIAN, /* Little endian counter */
+             &ctr);                    /* where to store the CTR state */
+
+	 ctr_encrypt(buffer,         /* plaintext */
+                buffer,         /* ciphertext */
+                sizeof(buffer), /* length of plaintext pt */
+                &ctr);          /* CTR state */
+
+    zeromem(key, sizeof(key));
+    zeromem(&ctr, sizeof(ctr));
+    return buffer;
+}
+//---------------------------------------------------------------------------
+//=====================AES Decryption in CTR mode=========================================
+unsigned char* AESctr_decrypt(unsigned char *buffer, unsigned char *key)
+{
+    unsigned char IV[32] = "bbcdefhij12345bbcdefhij12345aaa";
+    symmetric_CTR ctr;
+    int x;
+		register_cipher(&aes_desc);
+    ctr_start(
+             find_cipher("aes"),    /* index of desired cipher */
+             IV,                        /* the initial vector */
+             key,                       /* the secret key */
+             32,                        /* length of secret key (16 bytes) */
+             0,                         /* 0 == default # of rounds */
+             CTR_COUNTER_LITTLE_ENDIAN, /* Little endian counter */
+             &ctr);
+		ctr_setiv(IV,
+              32,
+              &ctr);
+		ctr_decrypt( buffer, /* ciphertext */
+								buffer, /* plaintext */
+								sizeof(buffer), /* length of plaintext */
+								&ctr); /* CTR state */
+  	ctr_done(&ctr);
+
+		zeromem(key, sizeof(key));
+    zeromem(&ctr, sizeof(ctr));
+    return buffer;
+}
+//--------------------------------------------------------------------------------------------
+
+//==============================================================================
+// print out a string char by char
+void print_string_hex(char * str, size_t len)
+{
+            // prints out the chunk
+        for (int i = 0; i < len; ++i)
+        {
+            printf("%02hhX ", str[i]);
+
+        }
+        // printf ("\n\n");
+}
+
+unsigned char * HMAC_update(unsigned char * HMAC_current, unsigned char * HMAC_aggregate)
+{
+    // *HMAC_aggregate = *HMAC_aggregate | *HMAC_current;
+    // concat into one string
+    string temp;
+    for (int i =0; i < MAXBLOCKSIZE; ++i)
+        temp.push_back(HMAC_current[i]);
+    for (int i =0; i < MAXBLOCKSIZE; ++i)
+        temp.push_back(HMAC_aggregate[i]);
+
+    // hash and return 
+    return hashSHA2(temp);
+
+}
+
+//==============================================================================
+// BEGIN MAIN
 int main(void)
 {
+    //==============================================================================
+    // BEGIN READ FILES
     // Alice reads the key
     std::ifstream key_file;
-    std::string key; //char buffer
+    std::string keyString; //char buffer
 
-    std::ifstream text_file;
-    std::stringstream text_data; //char buffer
+    std::ifstream Messages_file;
+    std::stringstream Messages; //char buffer
+    FILE * ciphertexts_fd = fopen("TheCiphertexts.txt", "w");
+    FILE * HMACs_fd = fopen("HMACs.txt", "w");
+    FILE * plaintext_hex_fd = fopen("Plaintext_hex_alice.txt", "w");
+    FILE * HMAC_aggregate_fd = fopen("AggregatedHMAC.txt", "w");
+    
 
-    std::ofstream TheCiphertext;
-
-    TheCiphertext.open("TheCiphertext.txt");
-
-    using namespace std;
-
-    // read in the secret
-    key_file.open("sharedSecret.txt");
+    // read in the secret key into "key"
+    // file name is hard coded
+    key_file.open("InitialKey.txt");
     if (key_file.is_open())
     {
-        key_file >> key;
+        key_file >> keyString;
     }
     key_file.close();
 
+    //copy key to unsigned char array
+    unsigned char * key_cstr = (unsigned char*) malloc(keyString.size());
+    unsigned char * key_cstr_next = (unsigned char*) malloc(keyString.size());
+    memcpy(key_cstr, keyString.c_str(), keyString.size());
+    memset(key_cstr_next, 0, MAXBLOCKSIZE);
+
+    //holds aggregate HMAC
+    unsigned char * HMAC_aggregate = (unsigned char *) malloc(MAXBLOCKSIZE);
+    // set to zero initially
+    memset(HMAC_aggregate, 0, MAXBLOCKSIZE);
+
     //read in the plain text
-    text_file.open("HW1PlaintextTest.txt");
-    // text_file.open("testText.txt", std::ios::binary);
+    // message data, 100 messages, 1024 bytes each
+    Messages_file.open("Messages.txt", ios::binary);
 
-    if (text_file.is_open())
+    if (Messages_file.is_open())
     {
-        text_data << text_file.rdbuf();
+        Messages << Messages_file.rdbuf();
     }
 
-    // hash all text and print to terminal
-    //holds the resulting hash
-    unsigned char *hashedinput = (unsigned char *)malloc(sha3_256_desc.hashsize);
-    //compute the hash
-    hash_state hs1;
-    sha3_256_init(&hs1);
-    sha3_process(&hs1, (const unsigned char *)text_data.str().c_str(), text_data.str().size());
-    sha3_done(&hs1, hashedinput); //holds the hash result
+    //==============================================================================
+    // BEGIN ENCRYPTION
 
-    // print each byte as hexadecimal to terminal
-    cout << "Hash of input in hex: \n";
-    for (size_t i = 0; i < sha3_256_desc.hashsize; ++i)
+    // split messages file into 1024byte messages 
+    unsigned CHUNK_SIZE = 1024;
+    size_t CHUNK_SIZE_BYTES = CHUNK_SIZE * sizeof(char);
+    // size_t currentChunk = 0;
+
+    vector<unsigned char> ciphertexts_vector;
+    vector<unsigned char> HMACs_vector;
+
+    // unsigned 
+
+    // for (size_t i = 0; i < Messages.str().size(); i += CHUNK_SIZE, currentChunk++)
+    for (size_t currentChunk = 0; currentChunk < Messages.str().size()/CHUNK_SIZE; ++currentChunk)
     {
-        printf("%2.2hx", hashedinput[i]);
-    }
-    cout << endl;
-
-    // will hold the cyphertext, allocate memory
-    size_t cyphertextSize = text_data.str().size() * sizeof(unsigned char);
-    unsigned char *cyphertext = (unsigned char *)malloc(cyphertextSize);
-
-    // will concat this with the hashed chunk to make the key
-    char concatChar = '1';
-
-    // split text into 32 byte chunks then XOR with each 32 byte key
-    unsigned CHUNK_SIZE = 32;
-    size_t currentChunk = 0;
-
-    for (size_t i = 0; i < text_data.str().size(); i += CHUNK_SIZE, currentChunk++)
-    {
+        // printf(" msg_len, CHUNK_SIZE_BYTES, currentChunk: %d %d %d\n\n", Messages.str().size(), CHUNK_SIZE_BYTES, currentChunk);
 
         std::string chunk;
-        chunk = text_data.str().substr(currentChunk * CHUNK_SIZE, ((currentChunk + 1) * CHUNK_SIZE) - 1);
+        chunk = Messages.str().substr(currentChunk * CHUNK_SIZE, CHUNK_SIZE);
+        
+        // convert std string to unsigned char array
+        unsigned char* chunk_cstr = (unsigned char *)malloc(CHUNK_SIZE_BYTES);
+        // memcpy(chunk_cstr,  (chunk.c_str()), CHUNK_SIZE_BYTES );
+        memcpy(chunk_cstr,  (&Messages.str().c_str()[currentChunk * CHUNK_SIZE]), CHUNK_SIZE_BYTES );
+        chunk_cstr = reinterpret_cast <unsigned char*>(chunk_cstr);
 
-        // Use SHA-256 to Hash
-        // This part requires libtomcrypt
-        unsigned char r1[256 / 8];
-        hash_state c;
-        // We have key||1 - || = concatenation
-        sha3_256_init(&c);
-        sha3_process(&c, (const unsigned char *)(key + concatChar).c_str(), (key + concatChar).size());
-        concatChar++;
-        // r1 contains the current key
-        sha3_done(&c, r1);
-
-        for (size_t x = 0; x < CHUNK_SIZE; ++x)
+        for(int i = 0; i < CHUNK_SIZE; ++i)
         {
-            // index within the cyphertext array
-            size_t cyphertextIdx = (CHUNK_SIZE * currentChunk) + x;
-            cyphertext[cyphertextIdx] = r1[x] ^ text_data.str()[cyphertextIdx];
+            printf("%02hhX", chunk_cstr[i]);
+            fprintf(plaintext_hex_fd, "%02hhX", chunk_cstr[i]);
         }
+        printf("\n");
+        fprintf(plaintext_hex_fd, "\n");
+
+
+        // convert std string to unsigned char array
+        // unsigned char* key_cstr = (unsigned char *)malloc(key.size());
+        // memcpy(key_cstr, key.c_str(), key.size() );
+        // key_cstr = reinterpret_cast <unsigned char*> (key_cstr);
+
+        // print key in hex, char by char
+        // print_string_hex(reinterpret_cast<char*>(key_cstr), key.size());
+        // printf ("\n\n");
+
+        unsigned char* ciphertext_buffer =  (unsigned char*)malloc(CHUNK_SIZE_BYTES);
+        memcpy(ciphertext_buffer, chunk_cstr, CHUNK_SIZE_BYTES);
+
+        //==============================================================================
+        // ENCRYPTION STEP        
+        // encrypts data in buffer then returns ciphertext
+        ciphertext_buffer = AESctr_encrypt(ciphertext_buffer, key_cstr);
+
+        // compute HMAC 
+        unsigned char HMAC[MAXBLOCKSIZE];
+        HMAC_Computation(ciphertext_buffer, HMAC, key_cstr);
+        HMAC_aggregate = HMAC_update(HMAC, HMAC_aggregate);
+
+        // add ciphertext to ciphertexts_vector
+        // push each unsigned char
+        for(int i =0; i < CHUNK_SIZE; ++i)
+        {
+            ciphertexts_vector.push_back(ciphertext_buffer[i]);
+            // printf("%02hhX", msg[i]);
+        }
+        // print HMAC to file
+        for (int i = 0; i < MAXBLOCKSIZE; ++i)
+        {
+            HMACs_vector.push_back(HMAC[i]);
+        }
+
+        
+        // free any mallocs here
+        free(chunk_cstr);
+        // free(key_cstr);
+        free(ciphertext_buffer);
+        // printf("===================================================================================\n");
     }
 
-    // print each byte as hexadecimal to filestream
-    for (size_t i = 0; i < cyphertextSize; ++i)
+    // send cipher text
+
+    // send HMAC
+
+
+    //==============================================================================
+    // write ciphertexts to file
+    // FILE * ciphertexts_fd = fopen("TheCiphertexts.txt", "w");
+    for (int i = 0; i < ciphertexts_vector.size(); ++i)
     {
-        TheCiphertext << hex << setw(2) << setfill('0') << int(cyphertext[i]);
+        fprintf(ciphertexts_fd, "%02hhX", ciphertexts_vector.data()[i]);
     }
-    printf("\n");
-    TheCiphertext.close();
 
-    // TheCiphertext.write(reinterpret_cast<const char *>(cyphertext), cyphertextSize);
+    // write HMACs to file
+    // FILE * HMACs_fd = fopen("HMACs.txt", "w");
+    for (int i = 0; i < HMACs_vector.size(); ++i)
+    {
+        fprintf(HMACs_fd, "%02hhX", HMACs_vector.data()[i]);
+    }
 
+    for (int i = 0; i < MAXBLOCKSIZE; ++i)
+    {
+        fprintf(HMAC_aggregate_fd, "%02hhX", HMAC_aggregate[i]);
+    }
+
+    //==============================================================================
+    // SEND DATA with ZMQ
     // connect to zmq server
     //  Prepare our context and socket
     zmq::context_t context(1);
@@ -109,19 +286,23 @@ int main(void)
     std::cout << "Connecting to server at tcp://localhost:5555..." << std::endl;
     socket.connect("tcp://localhost:5555");
 
-    // send the hash first
-    zmq::message_t request(hashedinput, sha3_256_desc.hashsize);
+    // send combined ciphertexts
+    zmq::message_t request(ciphertexts_vector.data(), ciphertexts_vector.size() );
     std::cout << "Sending Message... \n";
 
     // use ZMQ_SNDMORE to indicate another message is following
     // socket.send(request, ZMQ_SNDMORE);
-    socket.send(request, zmq::send_flags::sndmore);
+    // socket.send(request, zmq::send_flags::sndmore);
+    socket.send(request, zmq::send_flags::none);
 
     //send the cyphertext now
-    zmq::message_t request2(cyphertext, cyphertextSize);
-    socket.send(request2, zmq::send_flags::none);
+    // zmq::message_t request2(cyphertext, cyphertextSize);
+    // socket.send(request2, zmq::send_flags::none);
 
-    text_file.close();
+    Messages_file.close();
     key_file.close();
-    free(cyphertext);
+
+    fclose(ciphertexts_fd);
+    fclose(HMACs_fd);
+    free(HMAC_aggregate);
 }
--- bob.cpp
+++ bob.cpp
@@ -7,137 +7,262 @@
 #include <stdlib.h>
 #include <tomcrypt.h>
 #include <fstream>
+#include <stdio.h>
 
-int main(void)
+using namespace std;
+
+//===================SHA256 Function=========================================
+unsigned char* hashSHA2(const string& input)
 {
-    // Alice reads the key
+	unsigned char* hash_res = new unsigned char[sha256_desc.hashsize];
+    hash_state md;
+    sha256_init(&md);
+    sha256_process(&md, (const unsigned char*) input.c_str(), input.size());
+    sha256_done(&md, hash_res);
+    return hash_res;
+}
+//----------------------------------------------------------------------------------
+//===================HMAC Function based on SHA256=========================================
+// void HMAC_Computation(char *message, unsigned char *mac, unsigned char *key)
+void HMAC_Computation(unsigned char *message, unsigned char *mac, unsigned char *key)
+{
+    int idx;
+    hmac_state hmac;
+    unsigned char dst[MAXBLOCKSIZE];
+    unsigned long dstlen;
+    register_hash(&sha256_desc);
+    idx = find_hash("sha256");
+    hmac_init(&hmac, idx, key, 32);
+    hmac_process(&hmac, (const unsigned char*) message, sizeof(message));
+    dstlen = sizeof(dst);
+    hmac_done(&hmac, dst, &dstlen);
+    memcpy(mac, dst, dstlen);
+}
+//---------------------------------------------------------------------------------
+//===================AES Encryption in CTR mode=========================================
+unsigned char* AESctr_encrypt(unsigned char *buffer, unsigned char *key)
+{
+    // hardcoded IV
+    unsigned char IV[32] = "abcdefghijklmnopqrstuvwxyzabcde";
+		symmetric_CTR ctr;
+    int x;
+    /* register AES first */
+   register_cipher(&aes_desc);
+   ctr_start(
+             find_cipher("aes"),    /* index of desired cipher */
+             IV,                        /* the initial vector */
+             key,                       /* the secret key */
+             32,                        /* length of secret key (16 bytes) */
+             0,                         /* 0 == default # of rounds */
+             CTR_COUNTER_LITTLE_ENDIAN, /* Little endian counter */
+             &ctr);                    /* where to store the CTR state */
+
+	 ctr_encrypt(buffer,         /* plaintext */
+                buffer,         /* ciphertext */
+                sizeof(buffer), /* length of plaintext pt */
+                &ctr);          /* CTR state */
+
+    zeromem(key, sizeof(key));
+    zeromem(&ctr, sizeof(ctr));
+    return buffer;
+}
+//---------------------------------------------------------------------------
+//=====================AES Decryption in CTR mode=========================================
+unsigned char* AESctr_decrypt(unsigned char *buffer, unsigned char *key)
+{
+    // unsigned char IV[32] = "bbcdefhij12345bbcdefhij12345aaa";
+    unsigned char IV[32] = "abcdefghijklmnopqrstuvwxyzabcde";
+
+    symmetric_CTR ctr;
+    int x;
+		register_cipher(&aes_desc);
+    ctr_start(
+             find_cipher("aes"),    /* index of desired cipher */
+             IV,                        /* the initial vector */
+             key,                       /* the secret key */
+             32,                        /* length of secret key (16 bytes) */
+             0,                         /* 0 == default # of rounds */
+             CTR_COUNTER_LITTLE_ENDIAN, /* Little endian counter */
+             &ctr);
+		ctr_setiv(IV,
+              32,
+              &ctr);
+		ctr_decrypt( buffer, /* ciphertext */
+								buffer, /* plaintext */
+								sizeof(buffer), /* length of plaintext */
+								&ctr); /* CTR state */
+  	ctr_done(&ctr);
+
+		zeromem(key, sizeof(key));
+    zeromem(&ctr, sizeof(ctr));
+    return buffer;
+}
+//--------------------------------------------------------------------------------------------
+
+// print out a string char by char
+template <class T>
+void print_string_hex(T str, size_t len)
+{
+            // prints out the chunk
+        for (int i = 0; i < len; ++i)
+        {
+            printf("%02hhX ", str[i]);
+
+        }
+        // printf ("\n\n");
+}
+//==============================================================================
+
+//==============================================================================
+// BEGIN MAIN
+int main(){
+    //==============================================================================
+    // OPEN FILES
+
+    // open C file descriptors
+    FILE * HMACs_fd = fopen("HMACs_bob.txt", "w");
+    FILE * ciphertexts_fd = fopen("ciphertexts_bob.txt", "w");
+    FILE * plaintext_fd = fopen("Plaintext_bob.txt", "w");
+    FILE * plaintext_hex_fd = fopen("Plaintext_hex_bob.txt", "w");
+    FILE * HMAC_aggregate_fd = fopen("matchedAggregatedHMAC.txt", "w");
+
+    // Bob reads the key
     std::ifstream key_file;
     std::string key; //char buffer
-
-    std::ofstream Bob_h;
-    Bob_h.open("Bob_h.txt");
-
-    std::ofstream BobPlaintext;
-    BobPlaintext.open("BobPlaintext.txt");
-
-    // read in the secret
-    key_file.open("sharedSecret.txt");
-    if (key_file.is_open())
-    {
+    key_file.open("InitialKey.txt");
+    if(key_file.is_open() ) {
         key_file >> key;
     }
     key_file.close();
 
-    // connect to zmq server
-    // RECEIVE HERE
-    // Prepare our context and socket
-    zmq::context_t context(2);
-    zmq::socket_t socket(context, zmq::socket_type::rep);
-    socket.bind("tcp://*:5555");
-
-    // receive message hash
+    //==============================================================================
+    // RECEIVE CIPHERTEXT
+    //  Prepare our context and socket
+    zmq::context_t context (2);
+    zmq::socket_t socket (context, zmq::socket_type::rep);
+    socket.bind ("tcp://*:5555");
+    
+    // request will hold the recieved data
     zmq::message_t request;
     zmq::recv_result_t ret = socket.recv(request, zmq::recv_flags::none);
 
-    unsigned char *hashedMessage = (unsigned char *)malloc(ret.value());
-    memcpy(hashedMessage, request.data(), ret.value());
-    //  = static_cast<unsigned char *>(request.data());
+    // convert recieved data to unsigned char vector - identical to sent version
+    vector<unsigned char> ciphertexts(ret.value());
+    memcpy(ciphertexts.data(), request.data(), ret.value());
 
-    printf("Received Hash in hex: \n");
-    for (size_t i = 0; i < request.size(); i++)
-    {   
-        // NOTE hex will be zero padded
-        printf("%2.2hx", hashedMessage[i]);
-    }
-    printf("\n\n");
+    // print recvd ciphertexts to file
+    for(int i =0; i < ciphertexts.size(); ++i)
+        {
+            // ciphertexts_vector.push_back(msg[i]);
+            // printf("%02hhX", ciphertexts.data()[i]);
+            fprintf(ciphertexts_fd, "%02hhX",  ciphertexts.data()[i]);
+        }
+    
+    printf("recvd size: %d\n", ret.value());
+    
+    //==============================================================================
+    // process ciphertexts and compute HMACs
 
-    // receive cyphertext
-    zmq::message_t request2;
-    ret = socket.recv(request2, zmq::recv_flags::none);
-
-    unsigned char *cyphertext = (unsigned char *)malloc(ret.value());
-    memcpy(cyphertext, request2.data(), request2.size());
-    size_t cyphertextSize = ret.value();
-
-    //CONVERT BACK TO PLAIN TEXT
-
-    // will hold the plainText, allocate memory
-    size_t plainTextSize = cyphertextSize;
-    unsigned char *plainText = (unsigned char *)malloc(cyphertextSize);
-
-    // will concat this with the hashed chunk to make the key
-    char concatChar = '1';
-
-    // split text into 32 byte chunks then XOR with each 32 byte key
-    unsigned CHUNK_SIZE = 32;
+    // split messages file into 1024byte messages 
+    unsigned CHUNK_SIZE = 1024;
+    size_t CHUNK_SIZE_BYTES = (CHUNK_SIZE * sizeof(char));
     size_t currentChunk = 0;
 
-    for (size_t i = 0; i < plainTextSize; i += CHUNK_SIZE, currentChunk++)
+    // for (size_t i = 0; i < ciphertexts.size(); i += CHUNK_SIZE, currentChunk++)
+    for (size_t currentChunk = 0; currentChunk < ciphertexts.size()/CHUNK_SIZE; ++currentChunk)
     {
 
-        // Use SHA-256 to Hash
-        // This part requires libtomcrypt
-        unsigned char r1[256 / 8];
-        hash_state c;
-        // We have key||1 - || = concatenation
-        sha3_256_init(&c);
-        sha3_process(&c, (const unsigned char *)(key + concatChar).c_str(), (key + concatChar).size());
-        concatChar++;
-        // r1 contains the current key
-        sha3_done(&c, r1);
+        // printf("i, CHUNK_SIZE_BYTES, currentChunk: %d %d %d\n\n", i, CHUNK_SIZE_BYTES, currentChunk);
 
-        for (size_t x = 0; x < CHUNK_SIZE; ++x)
+        // 1024byte chunk of ciphertext
+        unsigned char * ciphertext_chunk = (unsigned char*)malloc(CHUNK_SIZE_BYTES);
+        memcpy(ciphertext_chunk, &(ciphertexts.data()[currentChunk * CHUNK_SIZE]), CHUNK_SIZE_BYTES);
+
+        // print_string_hex(ciphertext_chunk, CHUNK_SIZE_BYTES);
+
+        // convert KEY
+        // convert std string to unsigned char array
+        unsigned char* key_cstr = (unsigned char *)malloc(key.size());
+        memcpy(key_cstr, key.data(), key.size() );
+        key_cstr = reinterpret_cast <unsigned char*> (key_cstr);
+
+        // compute HMAC 
+        unsigned char HMAC[MAXBLOCKSIZE];
+        HMAC_Computation(ciphertext_chunk, HMAC, key_cstr);
+
+        // write HMAC to file
+        for (int i = 0; i < MAXBLOCKSIZE; ++i)
         {
-            // index within the cyphertext array
-            size_t cyphertextIdx = (CHUNK_SIZE * currentChunk) + x;
-            plainText[cyphertextIdx] = r1[x] ^ cyphertext[cyphertextIdx];
+            fprintf(HMACs_fd, "%02hhX", HMAC[i]);
         }
-    }
+        
+        //==============================================================================
+        // BEGIN DECRYPT
 
-    // WRITE PLAIN TEXT TO FILE
+        unsigned char * plaintext_chunk = (unsigned char *)malloc(CHUNK_SIZE_BYTES);
+        memcpy(plaintext_chunk, ciphertext_chunk, sizeof(ciphertext_chunk));
 
-    BobPlaintext.write(reinterpret_cast<char *>(plainText), plainTextSize);
+        plaintext_chunk = AESctr_decrypt(plaintext_chunk, key_cstr);
+        char * plaintext_chunk_cast = reinterpret_cast<char *>(plaintext_chunk);
 
-    // hash all text and print to terminal
-    //holds the resulting hash
-    unsigned char *hashedinput = (unsigned char *)malloc(sha3_256_desc.hashsize);
-    //compute the hash
-    hash_state hs1;
-    sha3_256_init(&hs1);
-    sha3_process(&hs1, (const unsigned char *)plainText, plainTextSize);
-    sha3_done(&hs1, hashedinput); //holds the hash result
-
-    // print each byte as hexadecimal to terminal
-    // check hash match
-    bool hash_match = true;
-    std::cout << "Hash of plaintext in hex: \n";
-    for (size_t i = 0; i < sha3_256_desc.hashsize; ++i)
-    {
-        printf("%2.2hx", hashedinput[i]);
-
-        if (hashedinput[i] != hashedMessage[i])
+        for (int i = 0; i < CHUNK_SIZE_BYTES; ++i )
         {
-            printf("hash mismatch at %ld\n plain text hash, received hash: %2.2hx %2.2hx", i, hashedinput[i], hashedMessage[i]);
-            hash_match = !hash_match;
-
+            fprintf(plaintext_fd, "%c", plaintext_chunk_cast[i]);
+            fprintf(plaintext_hex_fd, "%02hhX", plaintext_chunk[i]);
+            printf("%02hhX", plaintext_chunk[i]);
         }
+        printf("\n");
+        fprintf(plaintext_hex_fd, "\n");
+
+
+        free(ciphertext_chunk);
+        free(key_cstr);
+        free(plaintext_chunk);
+        // free(msg);
+        // free(HMAC);
+        // free(buffer);
+
+        // printf("===================================================================================\n");
     }
-    printf("\n\n");
-    if (hash_match)
-        printf("Hashes successfully match\n");
 
-    std::cout << std::endl;
 
-    //write hash to Bob_h.txt
-    // print each byte as hexadecimal to filestream
-    for (size_t i = 0; i < sha3_256_desc.hashsize; ++i)
-    {
-        Bob_h << std::hex << std::setw(2) << std::setfill('0') << int(hashedinput[i]);
-    }
 
-    key_file.close();
-    Bob_h.close();
-    BobPlaintext.close();
-    free(cyphertext);
-    free(hashedMessage);
+
+
+
+
+
+
+
+
+
+
+    // zmq::message_t request2;
+    
+    //  Wait for next request from client (remember- 2 parts)
+    // socket.recv (request2, zmq::recv_flags::none);
+    // std::string rpl2 = std::string(static_cast<char*>(request2.data()), request2.size());
+
+
+    // HMAC_Computation(rpl, mac, key);
+
+    // if (mac == rpl2){
+    //     std::cout << "The aggregated HMAC matches with the received one!" << std::endl;
+        
+    //     ofstream myfile;
+    //     myfile.open ("Plaintexts.txt");
+    //     myfile << AESctr_decrypt(rpl, key);
+    //     myfile.close();
+
+    //     ofstream myfile;
+    //     myfile.open ("matchedAggregatedHMAC.txt");
+    //     myfile << mac;
+    //     myfile.close();
+
+    // }else std::cout << "The Authentication Process Went Wrong" << std::endl; 
+
+    fclose(HMACs_fd);
+    fclose(plaintext_fd);
+    fclose(ciphertexts_fd);
+
 }
